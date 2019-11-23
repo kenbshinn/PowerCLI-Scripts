25c87a7f1c1f7cdec12e0718a978ee8ae00959ea
Ken Shinn <kenbshinn@gmail.com>
2019-11-23 18:00:43 -0500

    Update VM_Datastore_Report.ps1

--- VM_Datastore_Report.ps1
+++ VM_Datastore_Report.ps1
@@ -1,19 +1,11 @@
 #VM – Datastore Report
-
-# Specify to vCenter
+# Specify to vCenter that the VM is being hosted on. You can also point to a ESXi host. 
 
 $vCenter = Read-Host -Prompt 'Enter your vCenter here'
 
- 
-
 connect-viserver -server $vCenter
 
- 
-
-# Specify VM name
-
+# Specify VM name that you are checking the storage against
 $vm = Read-Host -Prompt 'Enter VM Name here'
-
- 
 
 get-datastore -vm $vm | Select Name, FreeSpaceGB, CapacityGB, @{N='CanonicalName';E={$_.ExtensionData.Info.Vmfs.Extent[0].DiskName}}
